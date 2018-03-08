418326155e430c7cc030d26bd0a00b22e7f6e34c
rahmona-henry <rahmona.henry@icloud.com>
2018-03-08 10:01:28 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('client'))
+
+app.get('/', (req, res) => res.send('Hello DubSpot'))
+app.listen(4000, () => console.log('Catching a ride on port 5000'))
